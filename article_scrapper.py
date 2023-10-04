09d7374c6b0ae764acbff1d515f12d179f95b289
Yishen Miao <mys721tx@gmail.com>
2023-10-04 14:55:22 -0700

    Adding article_scrapper.py
    Adds article_scrapper.py to retrieve urls of the in situ page.

---
+++ article_scrapper.py
@@ -0,0 +1,119 @@
+import argparse
+import time
+import sys
+from typing import List, Tuple, Optional, Generator
+from urllib.parse import urlparse, urljoin
+
+from lxml import etree
+import requests
+
+
+def get_base_url(url: str) -> str:
+    """
+    Extract the base URL from the given URL.
+
+    Args:
+    - url (str): The full URL from which to extract the base URL.
+
+    Returns:
+    - str: The base URL.
+    """
+    parsed_url = urlparse(url)
+    return f"{parsed_url.scheme}://{parsed_url.hostname}"
+
+
+def scrape_page(url: str, verify_ssl: bool = True) -> Tuple[List[str], Optional[str]]:
+    """
+    Scrape the provided webpage and retrieve article links.
+
+    Args:
+    - url (str): The URL of the webpage to scrape.
+    - verify_ssl (bool): Whether to verify SSL certificates.
+
+    Returns:
+    - Tuple[List[str], Optional[str]]: A tuple containing a list of article links and the URL of the next page (if available).
+    """
+    response = requests.get(url, verify=verify_ssl)
+    if response.status_code != 200:
+        raise Exception(
+            f"Failed to fetch URL. HTTP Status Code: {response.status_code}"
+        )
+
+    parser = etree.HTMLParser()
+    tree = etree.fromstring(response.content, parser)
+
+    base_url = get_base_url(url)
+
+    # Extract links inside articles and convert them to absolute URLs if they're relative
+    article_links = tree.xpath('//*[@id="informations"]/article/header/p/a/@href')
+    article_links = [urljoin(base_url, link) for link in article_links]
+
+    # Get the next page link and convert it to an absolute URL if it's relative
+    next_page_links = tree.xpath(
+        '//*[@id="informations"]/ul[@class="pagelinks"]/li[@class="pagelinks-next"]/a/@href'
+    )
+    next_page_url = urljoin(base_url, next_page_links[0]) if next_page_links else None
+
+    return article_links, next_page_url
+
+
+def scrape_website(
+    start_url: str, sleep_duration: float, verify_ssl: bool = True
+) -> Generator[str, None, None]:
+    """
+    Generator that yields article links from the website starting from the provided URL,
+    navigating through paginated content.
+
+    Args:
+    - start_url (str): The starting URL for the scraping process.
+    - sleep_duration (float): Duration to wait between requests in seconds.
+    - verify_ssl (bool): Whether to verify SSL certificates.
+
+    Yields:
+    - str: Article link found on the page.
+    """
+    current_url = start_url
+
+    while current_url:
+        article_links, next_page_url = scrape_page(current_url, verify_ssl)
+        for link in article_links:
+            yield link
+        current_url = next_page_url
+        time.sleep(sleep_duration)
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Web Scraper for paginated content")
+    parser.add_argument(
+        "start_url", type=str, help="The starting URL for the scraping process"
+    )
+    parser.add_argument(
+        "--sleep",
+        "-s",
+        type=float,
+        default=0.5,
+        help="Duration to wait between requests in seconds. Default is 0.5 seconds.",
+    )
+    parser.add_argument(
+        "--ignore-ssl",
+        "-i",
+        action="store_true",
+        help="Ignore SSL certificate verification.",
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        type=argparse.FileType("w"),
+        default=sys.stdout,
+        help="Output file where the results should be written. Default is stdout.",
+    )
+
+    args = parser.parse_args()
+
+    with args.output as output:
+        for link in scrape_website(args.start_url, args.sleep, not args.ignore_ssl):
+            output.write(f"{link}\n")
+
+
+if __name__ == "__main__":
+    main()
