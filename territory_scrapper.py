00951b952f8f2e80d73d7b740b010f170b409220
Yishen Miao <mys721tx@gmail.com>
2023-10-04 20:19:57 -0700

    Adding territory_scrapper.py
    Adds territory_scrapper.py to handle in situ data.

---
+++ territory_scrapper.py
@@ -0,0 +1,99 @@
+import argparse
+import sys
+import time
+import csv
+from typing import List, Tuple, Optional
+
+import requests
+from lxml import etree
+
+
+def get_expression_info(
+    url: str, verify_ssl: bool = True
+) -> Tuple[List[str], Optional[str]]:
+    """
+    Retrieve the "Expression Territory" and "Predicted Gene" from the specified URL.
+
+    Args:
+    - url (str): The URL containing the desired information.
+    - verify_ssl (bool): Whether to verify SSL certificates.
+
+    Returns:
+    - Tuple[List[str], Optional[str]]: A tuple containing a list of territories and the predicted gene or None if it's not reported.
+    """
+    response = requests.get(url, verify=verify_ssl)
+    if response.status_code != 200:
+        raise Exception(
+            f"Failed to fetch URL. HTTP Status Code: {response.status_code}"
+        )
+
+    parser = etree.HTMLParser()
+    tree = etree.fromstring(response.content, parser)
+
+    # Extracting the Expression Territory information
+    territories = tree.xpath(
+        '//*[@id="informations"]/div[3]/div/div[3]/div/div/table/tr/td[1]/a/text()'
+    )
+
+    # Extracting the Predicted Gene information
+    predicted_genes = tree.xpath(
+        '//*[@id="informations"]/div[3]/div/div[2]/div[3]/p/a/text()'
+    )
+    predicted_gene = predicted_genes[0] if predicted_genes else None
+
+    return territories, predicted_gene
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Web Scraper for Expression Territory and Predicted Gene information"
+    )
+    parser.add_argument(
+        "--input",
+        "-i",
+        type=argparse.FileType("r"),
+        default=sys.stdin,
+        help="Input file containing URLs to scrape. Default is stdin.",
+    )
+    parser.add_argument(
+        "--output",
+        "-o",
+        type=argparse.FileType("w"),
+        default=sys.stdout,
+        help="Output file to write the results in TSV format. Default is stdout.",
+    )
+    parser.add_argument(
+        "--insecure",
+        "-n",
+        action="store_true",
+        help="Ignore SSL certificate verification.",
+    )
+    parser.add_argument(
+        "--sleep",
+        "-s",
+        type=float,
+        default=0.5,
+        help="Duration to wait between requests in seconds. Default is 0.5 seconds.",
+    )
+
+    args = parser.parse_args()
+
+    with args.input as input_file, args.output as output:
+        writer = csv.writer(output, delimiter="\t")
+        # Writing the header
+        writer.writerow(["URL", "Gene", "Territory"])
+
+        for line in input_file:
+            url = line.strip()
+            territories, predicted_gene = get_expression_info(url, not args.insecure)
+
+            for territory in territories:
+                writer.writerow(
+                    [url, predicted_gene if predicted_gene else "None", territory]
+                )
+
+            time.sleep(args.sleep)
+
+
+if __name__ == "__main__":
+    main()
