43812f2bd68611ba918311d7a5f13eb1af16d694
Yishen Miao <mys721tx@gmail.com>
2023-10-06 15:14:27 -0700

    Adding stage parsing
    Adds stage parsing.

--- territory_scrapper.py
+++ territory_scrapper.py
@@ -8,18 +8,34 @@ import requests
 from lxml import etree
 
 
+def extract_info(tree: etree._ElementTree, xpath: str) -> Optional[str]:
+    """
+    Helper function to extract information using a given XPath.
+
+    Args:
+    - tree (etree._ElementTree): The parsed HTML tree.
+    - xpath (str): The XPath to extract information.
+
+    Returns:
+    - Optional[str]: Extracted information or None if not found.
+    """
+    info = tree.xpath(xpath)
+    return info[0] if info else None
+
+
 def get_expression_info(
     url: str, verify_ssl: bool = True
-) -> Tuple[List[str], Optional[str]]:
+) -> Tuple[Optional[str], Optional[str], List[str]]:
     """
-    Retrieve the "Expression Territory" and "Predicted Gene" from the specified URL.
+    Retrieve the "Stage", "Expression Territory", and "Predicted Gene" from the specified URL.
 
     Args:
     - url (str): The URL containing the desired information.
     - verify_ssl (bool): Whether to verify SSL certificates.
 
     Returns:
-    - Tuple[List[str], Optional[str]]: A tuple containing a list of territories and the predicted gene or None if it's not reported.
+    - Tuple[Optional[str], Optional[str], List[str]]: A tuple containing the stage, the predicted gene,
+      and a list of territories  or None if not reported.
     """
     response = requests.get(url, verify=verify_ssl)
     if response.status_code != 200:
@@ -30,18 +46,16 @@ def get_expression_info(
     parser = etree.HTMLParser()
     tree = etree.fromstring(response.content, parser)
 
-    # Extracting the Expression Territory information
+    # Extracting the information using the refactored function
+    stage = extract_info(tree, '//*[@id="informations"]/div[3]/div/div[2]/div[2]/p/text()')
     territories = tree.xpath(
         '//*[@id="informations"]/div[3]/div/div[3]/div/div/table/tr/td[1]/a/text()'
     )
-
-    # Extracting the Predicted Gene information
-    predicted_genes = tree.xpath(
-        '//*[@id="informations"]/div[3]/div/div[2]/div[3]/p/a/text()'
+    predicted_gene = extract_info(
+        tree, '//*[@id="informations"]/div[3]/div/div[2]/div[3]/p/a/text()'
     )
-    predicted_gene = predicted_genes[0] if predicted_genes else None
 
-    return territories, predicted_gene
+    return stage, predicted_gene, territories
 
 
 def main():
@@ -81,15 +95,22 @@ def main():
     with args.input as input_file, args.output as output:
         writer = csv.writer(output, delimiter="\t")
         # Writing the header
-        writer.writerow(["URL", "Gene", "Territory"])
+        writer.writerow(["URL", "Stage", "Gene", "Territory"])
 
         for line in input_file:
             url = line.strip()
-            territories, predicted_gene = get_expression_info(url, not args.insecure)
+            stage, predicted_gene, territories = get_expression_info(
+                url, not args.insecure
+            )
 
             for territory in territories:
                 writer.writerow(
-                    [url, predicted_gene if predicted_gene else "None", territory]
+                    [
+                        url,
+                        stage if stage else "None",
+                        predicted_gene if predicted_gene else "None",
+                        territory,
+                    ]
                 )
 
             time.sleep(args.sleep)
