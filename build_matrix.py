2769ed20319a38609d90b3e2349cdc0f1f66c450
Yishen Miao <mys721tx@gmail.com>
2023-10-25 08:08:28 -0700

    Refactoring padding and cosine similarity
    Refactors pad_compatible and get_cos_similarity to be used with both
    AnnData and DataFrame.

--- build_matrix.py
+++ build_matrix.py
@@ -1,7 +1,8 @@
-from typing import Dict
+from typing import Dict, Union, Tuple
 import numpy as np
 import pandas as pd
 import anndata
+from sklearn.preprocessing import normalize
 
 
 def preprocess_tsv(filepath: str, mapping_filepath: str) -> pd.DataFrame:
@@ -83,26 +84,54 @@ def build_from_df(df: pd.DataFrame) -> pd.DataFrame:
     return mat
 
 
-def pad_compatible(mat: pd.DataFrame, adata: anndata.AnnData) -> pd.DataFrame:
+def pad_compatible(
+    obj1: Union[anndata.AnnData, pd.DataFrame],
+    obj2: Union[anndata.AnnData, pd.DataFrame],
+) -> Tuple[Union[anndata.AnnData, pd.DataFrame], Union[anndata.AnnData, pd.DataFrame]]:
     """
-    Adjust the adjacency matrix to have the same number and order of columns as the AnnData object.
+    Adjust both input objects (either AnnData or DataFrame) to have the same columns.
 
     Parameters:
-    - mat (pd.DataFrame): Adjacency matrix with cells in rows and genes in columns.
-    - adata (anndata.AnnData): An AnnData object.
+    - obj1 (Union[anndata.AnnData, pd.DataFrame]): First object (either AnnData or DataFrame).
+    - obj2 (Union[anndata.AnnData, pd.DataFrame]): Second object (either AnnData or DataFrame).
 
     Returns:
-    - pd.DataFrame: Adjusted adjacency matrix.
+    - tuple: Tuple containing the adjusted objects.
     """
 
-    # Extract the gene names from the AnnData object's variable names
-    genes = adata.var_names.tolist()
+    # Convert AnnData to DataFrame if necessary
+    df1 = (
+        pd.DataFrame(obj1.X, columns=obj1.var_names, index=obj1.obs_names)
+        if isinstance(obj1, anndata.AnnData)
+        else obj1
+    )
+    df2 = (
+        pd.DataFrame(obj2.X, columns=obj2.var_names, index=obj2.obs_names)
+        if isinstance(obj2, anndata.AnnData)
+        else obj2
+    )
 
-    # Reindex the adjacency matrix to match the AnnData object's genes
-    # This will introduce NaN for missing genes which should be filled with zeros
-    mat_padded = mat.reindex(columns=genes).fillna(0).astype(int)
+    # Get union of columns from both dataframes
+    all_cols = df1.columns.union(df2.columns)
 
-    return mat_padded
+    # Reindex dataframes with all_cols and fill missing values with zeros
+    df1 = df1.reindex(columns=all_cols, fill_value=0)
+    df2 = df2.reindex(columns=all_cols, fill_value=0)
+
+    # Convert back to AnnData if original objects were AnnData
+    if isinstance(obj1, anndata.AnnData):
+        obj1.X = df1.values
+        obj1.var_names = all_cols
+    elif isinstance(obj1, pd.DataFrame):
+        obj1 = df1
+
+    if isinstance(obj2, anndata.AnnData):
+        obj2.X = df2.values
+        obj2.var_names = all_cols
+    elif isinstance(obj2, pd.DataFrame):
+        obj2 = df2
+
+    return obj1, obj2
 
 
 def split_adata(adata: anndata.AnnData, col: str) -> Dict[str, anndata.AnnData]:
@@ -121,37 +150,51 @@ def split_adata(adata: anndata.AnnData, col: str) -> Dict[str, anndata.AnnData]:
     return {value: adata[adata.obs[col] == value] for value in adata.obs[col].unique()}
 
 
-def get_cos_similarity(adata: anndata.AnnData, df: pd.DataFrame) -> pd.DataFrame:
+def get_cos_similarity(
+    obj1: Union[anndata.AnnData, pd.DataFrame],
+    obj2: Union[anndata.AnnData, pd.DataFrame],
+) -> pd.DataFrame:
     """
-    Calculate the cosine similarity matrix using the matrix product of the AnnData object's data and the transposed DataFrame.
+    Compute the cosine similarity between two objects (either AnnData or DataFrame).
 
     Parameters:
-    - adata (anndata.AnnData): Input AnnData object.
-    - df (pd.DataFrame): Input DataFrame.
+    - obj1 (Union[anndata.AnnData, pd.DataFrame]): First object (either AnnData or DataFrame).
+    - obj2 (Union[anndata.AnnData, pd.DataFrame]): Second object (either AnnData or DataFrame).
 
     Returns:
-    - pd.DataFrame: Cosine similarity matrix with row and column names restored and sorted.
+    - pd.DataFrame: A dataframe of cosine similarity values.
     """
 
-    # Ensure df and adata have compatible columns
-    df_padded = pad_compatible(df, adata)
-
-    # Convert the DataFrame to numpy for efficient matrix operations
-    df_np = df_padded.to_numpy()
-
-    # Normalize the matrices using L2 norm (axis=1 indicates row-wise normalization)
-    adata_norm = adata.X / np.linalg.norm(adata.X, axis=1)[:, np.newaxis]
-    df_norm = df_np / np.linalg.norm(df_np, axis=1)[:, np.newaxis]
-
-    # Calculate the dot product which gives cosine similarity
-    cos_sim = adata_norm.dot(df_norm.T)
-
-    # Convert the resulting numpy matrix back to a DataFrame
-    similarity_df = pd.DataFrame(
-        cos_sim, index=adata.obs_names, columns=df_padded.index
+    # Convert AnnData to DataFrame if necessary
+    df1 = (
+        pd.DataFrame(obj1.X, columns=obj1.var_names, index=obj1.obs_names)
+        if isinstance(obj1, anndata.AnnData)
+        else obj1
+    )
+    df2 = (
+        pd.DataFrame(obj2.X, columns=obj2.var_names, index=obj2.obs_names)
+        if isinstance(obj2, anndata.AnnData)
+        else obj2
     )
 
-    # Sort the row and column names
-    similarity_df = similarity_df.sort_index(axis=0).sort_index(axis=1).fillna(0)
+    # Ensure both dataframes have the same columns
+    df1, df2 = pad_compatible(df1, df2)
+
+    # Convert dataframes to numpy arrays
+    arr1 = df1.to_numpy()
+    arr2 = df2.to_numpy()
+
+    # Normalize both arrays using L2 norm
+    arr1 = normalize(arr1, axis=1, norm="l2")
+    arr2 = normalize(arr2, axis=1, norm="l2")
+
+    # Compute cosine similarity
+    similarity = arr1 @ arr2.T
+
+    # Convert the result back to a dataframe with appropriate row and column names
+    similarity_df = pd.DataFrame(similarity, index=df1.index, columns=df2.index)
+
+    # Sort the result
+    similarity_df = similarity_df.sort_index(axis=0).sort_index(axis=1)
 
     return similarity_df
