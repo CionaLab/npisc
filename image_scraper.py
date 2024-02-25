b19c5733a7f53350d47c3340118a7cf2908a1601
Yishen Miao <mys721tx@gmail.com>
2024-02-25 12:53:24 -0800

    Adding image_scraper.py
    Adds image_scraper.py.

---
+++ image_scraper.py
@@ -0,0 +1,73 @@
+import argparse
+import csv
+import sys
+import time
+
+from urllib.parse import urljoin
+
+from scraper import fetch_parse, download_file, get_base_url
+
+XPATH_IMAGE = '//div[contains(@class, "content_yellow")]/div/div[@id="picture_description"]/div[contains(@class, "mini_picture")]/a/@href'
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Web Scraper for in situ images table."
+    )
+    # TODO: remove argparse.FileType("r") and argparse.FileType("w") and use str
+    # as they are not safe.
+    parser.add_argument(
+        "--input",
+        "-i",
+        type=argparse.FileType("r"),
+        default=sys.stdin,
+        help="Input file containing URLs to scrape. Default is stdin.",
+    )
+    parser.add_argument(
+        "--output",
+        "-o",
+        type=argparse.FileType("w"),
+        default=sys.stdout,
+        help="Output file to write the results in TSV format. Default is stdout.",
+    )
+    parser.add_argument(
+        "--insecure",
+        "-n",
+        action="store_true",
+        help="Ignore SSL certificate verification.",
+    )
+    parser.add_argument(
+        "--sleep",
+        "-s",
+        type=float,
+        default=0.5,
+        help="Duration to wait between requests in seconds. Default is 0.5 seconds.",
+    )
+
+    args = parser.parse_args()
+
+    with args.input as input_file, args.output as output:
+        writer = csv.writer(output, delimiter="\t")
+        # Writing the header
+        writer.writerow(["URL", "Image_URL", "Image"])
+
+        for line in input_file:
+            url = line.strip()
+
+            tree = fetch_parse(url, False)
+
+            for i in tree.xpath(XPATH_IMAGE):
+                url_image = urljoin(get_base_url(url), i)
+                file_image = download_file(url_image, False)
+                writer.writerow(
+                    [
+                        url,
+                        url_image,
+                        file_image,
+                    ]
+                )
+
+            time.sleep(args.sleep)
+
+
+if __name__ == "__main__":
+    main()
--- scraper.py
+++ scraper.py
@@ -1,5 +1,8 @@
 from typing import List, Optional
 from urllib.parse import urlparse
+import hashlib
+import io
+import mimetypes
 
 import requests
 from lxml import etree
@@ -55,3 +58,42 @@ def extract_info(tree: etree._ElementTree, xpath: str) -> Optional[str]:
     """
     info = tree.xpath(xpath)
     return info[0] if info else None
+
+
+def download_file(url: str, verify_ssl: bool = True) -> str:
+    """
+    Download a file from a URL, save it to a StringIO buffer, compute its sha512 value and use it as the filename.
+
+    Args:
+    - url (str): The URL of the file to download.
+    - verify_ssl (bool): Whether to verify SSL certificates.
+
+    Returns:
+    - str: The filename of the downloaded file.
+    """
+    response = requests.get(url, verify=verify_ssl)
+    if response.status_code != 200:
+        raise Exception(
+            f"Failed to download file. HTTP Status Code: {response.status_code}"
+        )
+
+    # Save the file to a StringIO buffer
+    buffer = io.BytesIO(response.content)
+
+    # Compute the sha512 value of the file
+    sha512 = hashlib.sha512()
+    sha512.update(buffer.getvalue())
+    filename = sha512.hexdigest()
+
+    # Get the extension name from the MIME type
+    mime = response.headers.get("content-type")
+    extension = mimetypes.guess_extension(mime)
+
+    # Use the sha512 value as the filename and the extension from the MIME type
+    filename_with_extension = f"{filename}{extension}"
+
+    # Write the file to disk
+    with open(filename_with_extension, "wb") as f:
+        f.write(buffer.getvalue())
+
+    return filename_with_extension
