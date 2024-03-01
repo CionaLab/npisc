6303a6c2375a64c792a91c2695ecb4bd0ad6821e
Yishen Miao <mys721tx@gmail.com>
2024-03-01 12:23:45 -0800

    Adding make_db_lookup.py
    Adds make_db_lookup.py for the FINULE project.

---
+++ make_db_lookup.py
@@ -0,0 +1,52 @@
+# %%
+
+import sys
+import re
+import csv
+import argparse
+from collections import defaultdict
+import networkx as nx
+from networkx.algorithms.traversal.breadth_first_search import bfs_tree
+
+from ontology import (
+    build_graph,
+    node_to_leaves,
+    get_node_name,
+    group_leaves_by_time,
+    get_linked_lists,
+    parse_url,
+)
+
+# %%
+
+g_territory, g_stage = build_graph("developmental_ontology.txt")
+
+pattern_stages = re.compile(r"^Stage \d+[a-z]?")
+STAGES = [
+    (i, j, g_stage.nodes[i]["synonym"])
+    for l in get_linked_lists(g_stage)
+    for (i, j) in l
+    if pattern_stages.match(j)
+    if pattern_stages.match(j)
+]
+
+# %%
+with open('stages.tsv', 'w') as f:
+    writer = csv.writer(f, delimiter='\t')
+    writer.writerow(['Term', 'Stage', 'Name'])
+    for s in STAGES:
+        writer.writerow(s)
+
+# %%
+
+with open('territories.tsv', 'w') as f:
+    writer = csv.writer(f, delimiter='\t')
+    writer.writerow(['Term', 'Name'])
+    for i in g_territory.nodes.data("name"):
+        writer.writerow(i)
+
+# %%
+STAGES
+
+# %%
+list(g_territory.nodes.data("name"))
