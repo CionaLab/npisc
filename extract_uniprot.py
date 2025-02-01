a2d6751a8e626d927b4a6b0da6149e5cd17a2c95
Yishen Miao <mys721tx@gmail.com>
2025-01-31 22:49:02 -0800

    Adding GO annotation
    Adds GO annotation.

--- extract_uniprot.py
+++ extract_uniprot.py
@@ -6,6 +6,7 @@ import sys
 STATE_INITIAL = 0
 STATE_AC = 1
 STATE_DE = 2
+STATE_DR = 3
 
 # Parse command-line arguments
 parser = argparse.ArgumentParser(description="Extract UniProt data")
@@ -28,17 +29,19 @@ args = parser.parse_args()
 # Read UniProt records from the input file and write the data to the output file
 with args.input as input_file, args.output as output_file:
     writer = csv.writer(output_file)
-    writer.writerow(["uniprot", "fullname"])
+    writer.writerow(["uniprot", "fullname", "go"])
 
     state = STATE_INITIAL
     accession_number = ""
     full_name = ""
+    go = []
 
     for line in input_file:
         if line.startswith("ID"):
             state = STATE_AC
             accession_number = ""
             full_name = ""
+            go = []
         elif state == STATE_AC:
             if line.startswith("AC"):
                 accession_number = line.split()[1].split(";")[0]
@@ -46,7 +49,11 @@ with args.input as input_file, args.output as output_file:
         elif state == STATE_DE:
             if line.startswith("DE   RecName:"):
                 full_name = line.split("=")[1].split(";")[0]
-                writer.writerow([accession_number, full_name])
+                state = STATE_DR
+        elif state == STATE_DR:
+            if line.startswith("DR   GO; "):
+                _, go_acc, go_text, _ = line.split("; ")
+                go.append(f"{go_acc} {go_text}")
+            elif line.startswith("//"):
                 state = STATE_INITIAL
-        elif line.startswith("//"):
-            state = STATE_INITIAL
+                writer.writerow([accession_number, full_name, "; ".join(go)])
