2942794986279399f4f5f7bd92bd0ace53fade9d
Yishen Miao <mys721tx@gmail.com>
2025-01-31 23:17:46 -0800

    Migrating to pandas
    Migrates to pandas.

--- extract_uniprot.py
+++ extract_uniprot.py
@@ -1,6 +1,7 @@
 import argparse
-import csv
 import sys
+
+import pandas as pd
 
 # State machine constants
 STATE_INITIAL = 0
@@ -8,52 +9,53 @@ STATE_AC = 1
 STATE_DE = 2
 STATE_DR = 3
 
-# Parse command-line arguments
-parser = argparse.ArgumentParser(description="Extract UniProt data")
-parser.add_argument(
-    "-i",
-    "--input",
-    default=sys.stdin,
-    type=argparse.FileType("r"),
-    help="Input file path (default: stdin)",
-)
-parser.add_argument(
-    "-o",
-    "--output",
-    default=sys.stdout,
-    type=argparse.FileType("w"),
-    help="Output file path (default: stdout)",
-)
-args = parser.parse_args()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Extract UniProt data")
+    parser.add_argument(
+        "-i",
+        "--input",
+        default=sys.stdin,
+        type=argparse.FileType("r"),
+        help="Input file path (default: stdin)",
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        default=sys.stdout,
+        type=argparse.FileType("w"),
+        help="Output file path (default: stdout)",
+    )
+    args = parser.parse_args()
 
-# Read UniProt records from the input file and write the data to the output file
-with args.input as input_file, args.output as output_file:
-    writer = csv.writer(output_file)
-    writer.writerow(["uniprot", "fullname", "go"])
+    with args.input as input_file:
+        data = []
+        state = STATE_INITIAL
+        accession_number = ""
+        full_name = ""
+        go = []
 
-    state = STATE_INITIAL
-    accession_number = ""
-    full_name = ""
-    go = []
+        for line in input_file:
+            if line.startswith("ID"):
+                state = STATE_AC
+                accession_number = ""
+                full_name = ""
+                go = []
+            elif state == STATE_AC:
+                if line.startswith("AC"):
+                    accession_number = line.split()[1].split(";")[0]
+                    state = STATE_DE
+            elif state == STATE_DE:
+                if line.startswith("DE   RecName:"):
+                    full_name = line.split("=")[1].split(";")[0]
+                    state = STATE_DR
+            elif state == STATE_DR:
+                if line.startswith("DR   GO; "):
+                    _, go_acc, go_text, go_source = line.split("; ")
+                    go.append(f"{go_acc} {go_text} {go_source[:-1]}")
+                elif line.startswith("//"):
+                    state = STATE_INITIAL
+                    data.append([accession_number, full_name, go])
 
-    for line in input_file:
-        if line.startswith("ID"):
-            state = STATE_AC
-            accession_number = ""
-            full_name = ""
-            go = []
-        elif state == STATE_AC:
-            if line.startswith("AC"):
-                accession_number = line.split()[1].split(";")[0]
-                state = STATE_DE
-        elif state == STATE_DE:
-            if line.startswith("DE   RecName:"):
-                full_name = line.split("=")[1].split(";")[0]
-                state = STATE_DR
-        elif state == STATE_DR:
-            if line.startswith("DR   GO; "):
-                _, go_acc, go_text, _ = line.split("; ")
-                go.append(f"{go_acc} {go_text}")
-            elif line.startswith("//"):
-                state = STATE_INITIAL
-                writer.writerow([accession_number, full_name, "; ".join(go)])
+        df = pd.DataFrame(data, columns=["uniprot", "fullname", "go"])
+        df["go"] = df["go"].str.join("; ")
+        df.to_csv(args.output, index=False)
